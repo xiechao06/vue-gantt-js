23a9fbdb7262a51d9ac802463b1f2d3e66686c87
xiechao06 <xiechao06@gmail.com>
2018-10-19 16:49:55 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <gantt
+      :project="project"
+    ></gantt>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import gantt from './Gantt.vue'
+import { Project } from 'gantt-engine'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    gantt
+  },
+  data () {
+    return {
+      project: new Project('sample')
+        .addSubTask(t => t
+          .name('A')
+          .addSubTask(t => t
+            .name('AA')
+          )
+        )
+        .addSubTask('C')
+    }
   }
 }
 </script>
